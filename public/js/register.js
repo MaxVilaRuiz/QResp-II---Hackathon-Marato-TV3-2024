c38b572e4ffcd925ebd34abd46af7aff633ebb2a
Max Vilà Ruiz <max16506@gmail.com>
2024-12-21 10:43:02 +0100

    LocalStorage - Register

--- public/js/register.js
+++ public/js/register.js
@@ -4,15 +4,17 @@ function register() {
     let password = document.getElementById('password').value;
     let password_rep = document.getElementById('repeat-password').value;
 
-    if (password !== password_rep) {
-        alert('Les contrassenyes no coincideixen.');
-    }
+    let existing_user = localStorage.getItem(email);
+    if (existing_user) alert('Ja hi ha un usuari registrat amb aquest correu electrònic.');
+    else if (password !== password_rep) alert('Les contrassenyes no coincideixen.');
     else {
-        const credentials = {
-            username: email,
+        let credentials = {
+            username: name,
+            id: email,
             password: password
         }
-
-        localStorage.setItem("hola", credentials);
+        
+        localStorage.setItem(credentials.id, JSON.stringify(credentials));
+        window.location.href = "/public/html/userpage.html";
     }
 }
