415264aa48512779565c5fa01ce8c7525e753ebd
Max Vilà Ruiz <max16506@gmail.com>
2024-12-21 10:16:31 +0100

    Without Server

--- public/js/register.js
+++ public/js/register.js
@@ -1,4 +1,3 @@
-
 function register() {
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
@@ -8,36 +7,10 @@ function register() {
     if (password !== password_rep) {
         alert('Les contrassenyes no coincideixen.');
     }
-    else
-    {
+    else {
         const credentials = {
             username: email,
             password: password
         }
-
-        fetch('/api/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(credentials)
-        })
-        .then(response => response.json())
-        .then(data => {
-            if(data.success)
-            {
-                alert(data.message);
-                window.location.href = data.redirectUrl;
-            }
-            else
-            {
-                alert(data.message);
-            }
-        })
     }
-
-    console.log(name);
-    console.log(email);
-    console.log(password);
-    console.log(password_rep);
 }
